2996e4c1923e2e25c9245f0083ca6fdd1676e8a4
Ömer Faruk Birgül <farukbrgl@protonmail.com>
2022-02-10 23:41:02 +0300

    update

--- python/simeck.py
+++ python/simeck.py
@@ -13,27 +13,32 @@ bunlar orijinal makaledeki n=16, m=4 durumunda verilen başlangıç vektörleri
 sonuçların doğruluğunu makaleden inceleyebilirsiniz
 farklı boyuttaki şifreleyici için farklı değerler vermelisiniz
 """
-plainText_1 = 0x656b696c #most significant
-plainText_2 = 0x20646e75 #least significant
-print (format(plainText_1, '016X'), format(plainText_2, "016X"), "plaintexts in start in hex")
+plainText_1 = 0x656b696c  # most significant
+plainText_2 = 0x20646e75  # least significant
+print(format(plainText_1, '016X'), format(
+    plainText_2, "016X"), "plaintexts in start in hex")
 
 cipherText_2 = 0x45ce6902
 cipherText_1 = 0x5f7ab7ed
-print (format(cipherText_1, '016X'), format(cipherText_2, "016X"), "ciphertexts in start in hex")
+print(format(cipherText_1, '016X'), format(
+    cipherText_2, "016X"), "ciphertexts in start in hex")
 
-key_3 = 0x1b1a1918 #most significant
-key_2 = 0x13121110
-key_1 = 0x0b0a0908
-key_0 = 0x03020100 #least significant
-print (format(key_3, '016x'), format(key_2, "016X"), format(key_1, "016X"), format(key_0, "016X"),"keys in hex")
+t_2 = 0x1b1a1918  # most significant
+t_1 = 0x13121110
+t_0 = 0x0b0a0908
+k_0 = 0x03020100  # least significant
+# print(format(key_3, '016x'), format(key_2, "016X"), format(
+#     key_1, "016X"), format(key_0, "016X"), "keys in hex")
 
-#z sabitleri oluşturuluyor
-z_0 = [1,1,1,1,1,0,1,0,0,0,1,0,0,1,0,1,0,1,1,0,0,0,0,1,1,1,0,0,1,1,0,1,1,1,1,1,0,1,0,0,0,1,0,0,1,0,1,0,1,1,0,0,0,0,1,1,1,0,0,1,1,0]
-z_1 = [1,0,0,0,1,1,1,0,1,1,1,1,1,0,0,1,0,0,1,1,0,0,0,0,1,0,1,1,0,1,0,1,0,0,0,1,1,1,0,1,1,1,1,1,0,0,1,0,0,1,1,0,0,0,0,1,0,1,1,0,1,0]
+# z sabitleri oluşturuluyor
+z_0 = [1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1,
+       0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0]
+z_1 = [1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1,
+       0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0]
 
 
-#n ve m değerleri oluşturuluyor
-#bir alttaki bölümde el ile n ve m değeri girmeyi ayarlanabilir
+# n ve m değerleri oluşturuluyor
+# bir alttaki bölümde el ile n ve m değeri girmeyi ayarlanabilir
 n = 32
 m = 4
 
@@ -45,7 +50,7 @@ bu değerlere göre T ve j oluşturuluyor
 # n = int(input("n değerini giriniz:"))
 # m = int(input("m değerini giriniz:"))
 
-##sabit değer
+# sabit değer
 c = 2**(n) - 4
 
 
@@ -60,63 +65,64 @@ else:
     print("yanlış değer girdiniz\ndevam etmeyiniz")
 
 
-key_0_list= []
-def key_generation(key_0, key_1, key_2, key_3, m, T):
-    key_0_n = key_0
-    key_1_n = key_1
-    key_2_n = key_2
-    key_3_n = key_3
+# round function
+
+
+def round(pt_1, pt_2, key_round):
+    # plaintext = t1[0:n-1]t2[0:n-1]
+    t1 = plainText_1
+    t2 = plainText_2
+    # print ("before texts 01 in hex",format(t1, '04x'), format(t2, "04X"))
+    # print(key_0_list[i])
+    crol_1 = (t1 << 1) + (t1 >> (n - 1)) & c
+    crol_5 = (t1 << 5) + (t1 >> (n - 5)) & c
+    tmp1 = t1 & crol_5
+    tmp2 = t2 ^ tmp1
+    tmp3 = tmp2 ^ crol_1
+    tmp4 = tmp3 ^ key_round
+    t2 = t1
+    t1 = tmp4
+    # text1_list.append(t1)
+    # text2_list.append(t2)
+    plainText_1 = t1
+    plainText_2 = t2
+    return t1, t2
+    # print(format(text1_list[-1], '016X'),
+    #       format(text2_list[-1], "016X"), "ciphertext hex",)
+
+
+key_0_list = []
+
+
+def key_generation(t_2, t_1, t_0, k_0, T):
     # print (key_0_n)
     # print ("key_0_n in hex",format(key_0_n, '016b'))
-    for i in range(T):
-        # print ("key{} = {}".format(i, key_0_n))
-        # print ("key_0_n in hex",format(key_0_n, '016b'))
-        temp1 = (key_3 >> 3)|(key_3 << (n - 3)) & c
-        temp2 = key_1 ^ temp1
-        temp3 = (temp2 >> 1)|(temp2 << (n - 1)) & c
-        temp4 = key_0_n ^ temp2
-        temp5 = temp3 ^ temp4
-        if (n == 16 & n == 24):
-            temp6 = temp5 ^ c ^ ((z_0[i % 62]))
-        elif (n == 32):
-            temp6 = temp5 ^ c ^ ((z_1[i % 62]))
-        key_next = key_0_n
-        key_0_n = key_1
-        key_1 = key_2
-        key_2 = key_3
-        key_3 = temp6
-        key_0_list.append(key_next)
-        # print ("key{} = {}".format(i, key_next)
-    return key_0_list
+    # for i in range(T):
+    if (n == 16 & n == 24):
+        key_r = c ^ ((z_0[i % 62]))
+    elif (n == 32):
+        key_r = c ^ ((z_1[i % 62]))
+    t_0, k_0 = round(t_0, k_0, key_r)
+    temp = t_2
+    t_2 = t_0
+    t_0 = t_1
+    t_1 = temp
+    # key_0_list.append(key_next)
+    # print ("key{} = {}".format(i, key_next)
+    return k_0
 
-key_list =key_generation(key_0, key_1, key_2, key_3, m, T)
-text1_list= []
-text2_list= []
 
-### encryption
-def enc(plainText_1, plainText_2):
-    for k in key_0_list:
-        ### plaintext = t1[0:3]t2[0:3]
-        t1 = plainText_1
-        t2 = plainText_2
-        # print ("before texts 01 in hex",format(t1, '04x'), format(t2, "04X"))
-        # print(key_0_list[i])
-        crol_1 = (t1 << 1) + (t1 >> (n-1)) & c
-        crol_5 = (t1 << 5) + (t1 >> (n-5)) & c
-        tmp1 = t1 & crol_5
-        tmp2 = t2 ^ tmp1
-        tmp3 = tmp2 ^ crol_1
-        tmp4 = tmp3 ^ k
-        t2 = t1
-        t1 = tmp4
-        text1_list.append(t1)
-        text2_list.append(t2)
-        plainText_1 = t1
-        plainText_2 = t2
-        return text1_list, text2_list
-    print (format(text1_list[-1], '016X'), format(text2_list[-1], "016X"), "ciphertext hex",)
-print(text1_list)
+for i in range(T):
+    key = key_generation(t_2, t_1, t_0, k_0, T)
+    ct1, ct2 = round(plainText_1, plainText_2, key)
+    print(ct1, ct2)
 
+# key_list = key_generation(key_0, key_1, key_2, key_3, m, T)
+# # text1_list = []
+# # text2_list = []
+
+
+# print(text1_list)
 
 
 # ### decryption
@@ -153,7 +159,6 @@ print(text1_list)
 # # print(t1,t2)
 #     # return text1_list, text2_list
 # print (format(cp_text2_list[-1], '016X'), format(cp_text1_list[-1], "016X"), "plaintext hex")
-
 
 
 # key_generation(key_0, key_1, key_2, key_3, m)
