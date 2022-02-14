ef41ec33aff754a01340a3adec4a3be204f246f9
Ömer Faruk Birgül <birgulo@itu.edu.tr>
2022-02-14 13:41:21 +0300

    update

--- python/simeck_prng.py
+++ python/simeck_prng.py
@@ -12,7 +12,7 @@ a, b = simeck.SIMECK(plainText_1=IV_1, plainText_2=IV_2)
 x = format(a, '032b')
 y = format(b, '032b')
 f.write(str(x) + str(y) + "\n")
-for i in range(2):
+for i in range(9999):
     a, b = simeck.SIMECK(plainText_1=a, plainText_2=b)
     z = format(a, '032b')
     t = format(b, '032b')
